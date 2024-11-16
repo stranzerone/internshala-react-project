cd1e56f95b57386b1ee9343b20634c4a3d7c7938
sahil mulani <113999437+stranzerone@users.noreply.github.com>
2024-11-16 09:19:28 +0530

    store

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './redux/Store.js';
+import { store } from './redux/store.js';
 
 createRoot(document.getElementById('root')).render(
 
